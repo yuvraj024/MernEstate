448b94fade2bddb97c9d9580b06f9eafda2a7889
yuvraj024 <oberoiyuvraj024@gmail.com>
2023-11-04 16:45:56 +0530

    header updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import SignIn from './pages/SignIn';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
+import Header from './components/Header';
 export default function App() {
   return <BrowserRouter>
+  <Header/> 
   <Routes>
     <Route path="/" element={<Home />}/>
     <Route path="/sign-in" element={<SignIn />}/>
