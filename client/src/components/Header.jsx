0a201ae870aab9d3a2bb45d41c0213704a17d328
yuvraj024 <oberoiyuvraj024@gmail.com>
2023-11-14 11:10:09 +0530

    complete signup page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div className='flex justify-between items-center max-w-7xl mx-auto p-3'>
             <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                <span className='text-slate-500'>Yuvraj</span>
+                <span className='text-slate-500'>Mern</span>
                 <span className='text-slate-700'>Estate</span>
             </h1>
             </Link>
