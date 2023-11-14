57c8d044f0c94d7ff439ad412817369dfd30309d
yuvraj024 <oberoiyuvraj024@gmail.com>
2023-11-14 10:39:20 +0530

    create a middleware and a function to handle possible error

--- api/index.js
+++ api/index.js
@@ -13,6 +13,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app=express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
@@ -23,6 +24,16 @@ app.listen(3000,()=>{
 //     res.send("Hello Motto!");
 // })
 // by default json send ni kr skte server me isle
-app.use(express.json());
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+
+// middleware route
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+});
