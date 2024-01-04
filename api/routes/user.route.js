b16d4ad7394bf7a26a6436d3fd3e1d72472620f6
yuvraj024 <oberoiyuvraj024@gmail.com>
2024-01-04 20:22:07 +0530

    create get user listing

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
  import express from "express";
- import { test, updateUser } from '../controllers/user.controller.js';
+ import { test, updateUser,  getUserListings} from '../controllers/user.controller.js';
  import { verifyToken } from '../utils/verifyUser.js';
  const router=express.Router();
 
  router.get('/test',test)
  router.post('/update/:id', verifyToken, updateUser)
+ router.get('/listings/:id', verifyToken, getUserListings)
  export default router;
