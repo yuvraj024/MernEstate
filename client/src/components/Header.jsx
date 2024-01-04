fe2dc557a0bb6eac820a6616e8cc877ded69b5f3
yuvraj024 <oberoiyuvraj024@gmail.com>
2024-01-04 11:54:59 +0530

    updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-7xl mx-auto p-3'>
+        <div className='flex justify-between items-center px-5 sm:px-20 mx-auto p-3'>
             <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                 <span className='text-slate-500'>Mern</span>
